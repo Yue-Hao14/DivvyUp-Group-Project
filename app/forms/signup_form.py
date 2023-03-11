c8702d9ed42007eb7a8231969306da30717817df
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-03-11 15:51:13 -0500

    added firstName and lastName to sign up form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -23,5 +23,9 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
+    firstName = StringField(
+        'First Name', validators=[DataRequired()])
+    lastName = StringField(
+        'Last Name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -67,7 +67,7 @@ export const logout = () => async (dispatch) => {
 	}
 };
 
-export const signUp = (username, email, password) => async (dispatch) => {
+export const signUp = (username, firstName, lastName, email, password) => async (dispatch) => {
 	const response = await fetch("/api/auth/signup", {
 		method: "POST",
 		headers: {
@@ -75,6 +75,8 @@ export const signUp = (username, email, password) => async (dispatch) => {
 		},
 		body: JSON.stringify({
 			username,
+			firstName,
+			lastName,
 			email,
 			password,
 		}),
