3ce7251dcd9a9f2ce74f632be552bb30389d2d5d
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-03-11 15:31:37 -0500

    added schema to the 2 join tables

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,10 +20,10 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_friends()
         undo_comments()
         undo_expense_owers()
         undo_expenses()
-        undo_friends()
         undo_users()
     seed_users()
     seed_expenses()
@@ -36,9 +36,9 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_friends()
     undo_comments()
     undo_expense_owers()
     undo_expenses()
-    undo_friends()
     undo_users()
     # Add other undo functions here
