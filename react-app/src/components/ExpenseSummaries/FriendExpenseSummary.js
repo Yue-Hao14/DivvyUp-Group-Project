369a54ed81cece6461e8038e29ce8ea1394b326f
Troy Lee <troylee@berkeley.edu>
2023-03-14 16:48:36 -0700

    fixed an issue where clicking a friend would crash the site

--- react-app/src/components/ExpenseSummaries/FriendExpenseSummary.js
+++ react-app/src/components/ExpenseSummaries/FriendExpenseSummary.js
@@ -14,17 +14,20 @@ function FriendExpenseSummary({ expenses }) {
                 // filter owers to find the first instance of either the user or the friend
                 let ower = expense.owers.find(ower => (ower.id === sessionUser.id || ower.id === parseInt(friendId)))
                 return (
-                    <div key={expense.id} className="expense_summary_details_container">
-                        <div className="expense_summary_expense_date">{expense.expenseDate}</div>
-                        <div className="expense_summary_expense_icon">icon</div>
-                        <div className="expense_summary_expense_description">{expense.description}</div>
-                        <div className="expense_summary_expense_payerInfo">{expense.payer.firstName} {expense.payer.lastName[0]}. paid <span className="expense_summary_amount">${expense.amount}</span></div>
-                        <div className="expense_summary_expense_payerInfo">{ower.firstName} {ower.lastName[0]}. owes <span className="expense_summary_amount">${splitAmount}</span></div>
-                    </div>
+                    ower && (
+                        <div key={expense.id} className="expense_summary_details_container">
+                            <div className="expense_summary_expense_date">{expense.expenseDate}</div>
+                            <div className="expense_summary_expense_icon">icon</div>
+                            <div className="expense_summary_expense_description">{expense.description}</div>
+                            <div className="expense_summary_expense_payerInfo">{expense.payer.firstName} {expense.payer.lastName[0]}. paid <span className="expense_summary_amount">${expense.amount}</span></div>
+                            <div className="expense_summary_expense_payerInfo">{ower.firstName} {ower.lastName[0]}. owes <span className="expense_summary_amount">${splitAmount}</span></div>
+                        </div>
+                    )
                 )}
             )}
         </div>
     )
 }
+
 
 export default FriendExpenseSummary;
