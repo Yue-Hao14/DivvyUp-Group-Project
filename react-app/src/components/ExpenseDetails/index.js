f5651efbb908b9900f053b2c1657966d5b96ce3d
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-13 22:55:16 -0500

    add RESET action to expense reducer

--- react-app/src/components/ExpenseDetails/index.js
+++ react-app/src/components/ExpenseDetails/index.js
@@ -1,6 +1,11 @@
+import { useDispatch } from "react-redux"
+
 function ExpenseDetails(expenseId) {
     // dispatch thunk to get spot details added to store
+    const dispatch = useDispatch();
 
+    // check to make sure there is a logged in user and that the
+    // expense is in all of their expenses
     return (
         null
     )
