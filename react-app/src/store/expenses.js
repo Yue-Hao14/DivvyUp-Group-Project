34db0ed5b55c7c6371073af8b1014e4cf49133ff
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-13 22:39:28 -0500

    add full CRUD slices of state for expenses in react-app/src/store/expenses

--- react-app/src/store/expenses.js
+++ react-app/src/store/expenses.js
@@ -1,6 +1,8 @@
-const GET_ALL_EXPENSE = 'expenses/GET_ALL_EXPENSE'
-const GET_SINGLE_EXPENSE = 'expenses/GET_SINGLE_EXPENSE'
+const GET_ALL_EXPENSE = 'expenses/GET_ALL_EXPENSES'
+const GET_SINGLE_EXPENSE_DETAILS = 'expenses/GET_SINGLE_EXPENSE_DETAILS'
+const GET_SETTLED_EXPENSES = 'expenses/GET_SETTLED_EXPENSES'
 const ADD_EXPENSE = 'expenses/ADD_EXPENSE'
+const UPDATE_EXPENSE = 'expenses/UPDATE_EXPENSE'
 const DELETE_EXPENSE = 'expenses/DELETE_EXPENSE'
 
 
@@ -11,9 +13,29 @@ const getAllExpenses = expenses => ({
     payload: expenses
 })
 
-const getSingleExpense = expense => ({
-    type: GET_SINGLE_EXPENSE,
+const getSettledExpenses = expenses => ({
+    type: GET_SETTLED_EXPENSES,
+    payload: expenses
+})
+
+const getSingleExpenseDetails = expenseDetails => ({
+    type: GET_SINGLE_EXPENSE_DETAILS,
+    payload: expenseDetails
+})
+
+const postExpense = expense => ({
+    type: ADD_EXPENSE,
     payload: expense
+})
+
+const updateExpense = expense => ({
+    type: UPDATE_EXPENSE,
+    payload: expense
+})
+
+const deleteExpense = expenseId => ({
+    type: DELETE_EXPENSE,
+    id: expenseId
 })
 
 
@@ -35,12 +57,29 @@ export const getAllExpensesThunk = () => async (dispatch) => {
     }
 }
 
-export const getSingleExpenseThunk = (id) => async (dispatch) => {
+export const getSettledExpensesThunk = () => async (dispatch) => {
+    const res = await fetch('/api/expenses/settled')
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(getSettledExpenses(data));
+        return data;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors
+        }
+    } else {
+        return ["An Error occured. Please try again later"]
+    }
+}
+
+export const getSingleExpenseDetailsThunk = (id) => async (dispatch) => {
     const res = await fetch(`/api/expenses/${id}`)
 
     if (res.ok) {
-        const data = await response.json();
-        dispatch(getSingleExpense(data))
+        const data = await res.json();
+        dispatch(getSingleExpenseDetails(data))
         return data;
     } else if (res.status < 500) {
         const data = await res.json();
@@ -52,9 +91,69 @@ export const getSingleExpenseThunk = (id) => async (dispatch) => {
     }
 }
 
+export const postExpenseThunk = expense => async (dispatch) => {
+    const res = await fetch('/api/expenses/', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(expense)
+    })
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(postExpense(data));
+        return data;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ["An Error occured. Please try again later."]
+    }
+}
+
+export const updateExpenseThunk = expense => async (dispatch) => {
+    const res = await fetch(`/api/expenses/${expense.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(expense)
+    })
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(postExpense(data));
+        return data;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ["An Error occured. Please try again later."]
+    }
+}
+
+export const deleteExpenseThunk = expenseId => async (dispatch) => {
+    const res = await fetch(`/api/expenses/${expenseId}`, {
+        method: "DELETE"
+    })
+
+    if (res.ok) {
+        dispatch(deleteExpense(expenseId));
+        return null;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ["An Error occured. Please try again later."]
+    }
+}
+
 //reducer
 
-const initialState = {allExpenses : {}, singleExpense : {}}
+const initialState = {allExpenses : {}, settledExpenses: {}, currentExpenseDetails: {}}
 export default function reducer(state = initialState, action) {
     switch(action.type) {
         case GET_ALL_EXPENSE: {
@@ -64,10 +163,32 @@ export default function reducer(state = initialState, action) {
             }
             return {...state, allExpenses: expenses}
         }
-        case GET_SINGLE_EXPENSE: {
-            let newState = {...state}
-            newState.singleExpense = action.payload
+        case GET_SINGLE_EXPENSE_DETAILS: {
+            return {...state, currentExpenseDetails: action.payload}
+        }
+        case GET_SETTLED_EXPENSES: {
+            const settledExpenses = {}
+            for (const expense of action.payload) {
+                settledExpenses[expense.expenseId] = expense
+            }
+            return {...state, settledExpenses};
+        }
+        case ADD_EXPENSE: {
+            const newState = {...state};
+            newState.allExpenses = { ...state.allExpenses, [action.payload.id]: action.payload };
             return newState;
+        }
+        case UPDATE_EXPENSE: {
+            const newState = { ...state };
+            newState.allExpenses = { ...state.allExpenses, [action.payload.id]: action.payload };
+            newState.currentExpenseDetails = action.payload;
+            return newState;
+        }
+        case DELETE_EXPENSE: {
+            const newState = { ...state }
+            newState.allExpenses = { ...state.allExpenses }
+            delete newState.allExpenses[action.id]
+            return newState
         }
         default:
             return state
