80f7e8fc7cedec6117b524210af74612d0b99d1c
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-15 13:01:05 -0500

    add LoggedOutSplashPage component

---
+++ react-app/src/components/LoggedOutSplashPage/index.js
@@ -0,0 +1,10 @@
+function LoggedOutSplashPage() {
+    return (
+        <>
+            <h1>Welcome to DivvyUp!</h1>
+            <h2>Let us tell you what is so great about our app!</h2>
+        </>
+    )
+}
+
+export default LoggedOutSplashPage;
