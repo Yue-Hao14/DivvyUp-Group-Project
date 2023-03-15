6dc48b8b992bc223250abf040f8a46619e5df42c
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-15 16:01:43 -0500

    working on getting all settled user expense summaries

--- react-app/src/components/SettledExpenses/index.js
+++ react-app/src/components/SettledExpenses/index.js
@@ -1,9 +1,22 @@
+import { groupExpensesByMonth } from "../../utils/expenseHelpers";
+import ExpenseSummarySection from "./ExpenseSummarySection";
+
 function SettledExpenses() {
+    const settledExpenses = [];
+    // go through all user expenses
+        // if user is payer
+            // make sure all owers have paid (ie. length of owers matchs length of settledOwers)
+        // if user is not payer
+            // make sure they are in list of settled owers
+    const orderedSettledExpenses = groupExpensesByMonth(settledExpenses);
     return (
-        <>
-            <h1>Hello from SettledExpenses component</h1>
-            <h2>This is where all of the users settled expenses will be rendered</h2>
-        </>
+        <div className="expense_summaries_div">
+            {orderedSettledExpenses.map((expenseList, idx) => {
+                return (
+                    <ExpenseSummarySection key={idx} expenses={expenseList} />
+                )
+            })}
+        </div>
     )
 }
 
