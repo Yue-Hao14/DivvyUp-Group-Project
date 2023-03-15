c78046a2a5e55c692d936abdf6219088a96590fa
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-15 12:17:05 -0500

    create util function to formate date obj, and use to format createdAt and updatedAt in ExpenseDetails component

--- react-app/src/components/ExpenseDetails/index.js
+++ react-app/src/components/ExpenseDetails/index.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
+import { getMMDDYYYY } from "../../utils/utils";
 import EditAndDeleteButtons from "./EditAndDeleteButtons"
 import ExpenseUserInfo from "./ExpenseUserInfo";
 import './ExpenseDetails.css'
 
 function ExpenseDetails({ expense }) {
+    const createdAt = getMMDDYYYY(new Date(expense.createdAt))
+    const updatedAt = getMMDDYYYY(new Date(expense.updatedAt))
+
     return (
             <div className="expense_details_div">
                 <div className="expense_details_div_upper">
@@ -12,8 +16,8 @@ function ExpenseDetails({ expense }) {
                     <div className="expense_details_info_div">
                         <div className="expense_details_info_expense_description">{expense.description}</div>
                         <div className="expense_details_info_expense_amount">{expense.amount}</div>
-                        <div className="expense_details_info_added_by">{`Added by ${expense.payer.firstName} ${expense.payer.lastName[0]}. on ${expense.createdAt}`}</div>
-                        <div className="expense_details_info_last_updated">{`Last updated by ${expense.payer.firstName} ${expense.payer.lastName[0]}. on ${expense.updatedAt}`}</div>
+                        <div className="expense_details_info_added_by">{`Added by ${expense.payer.firstName} ${expense.payer.lastName[0]}. on ${createdAt}`}</div>
+                        <div className="expense_details_info_last_updated">{`Last updated by ${expense.payer.firstName} ${expense.payer.lastName[0]}. on ${updatedAt}`}</div>
                     </div>
                     <EditAndDeleteButtons expense={expense} />
                 </div>
