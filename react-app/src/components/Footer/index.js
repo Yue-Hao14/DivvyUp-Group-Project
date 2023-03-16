d7256fbdef82b014b84c77a6ed5e8b744208f1bc
kutun0901 <tuan.tq2433@gmail.com>
2023-03-16 14:20:08 -0700

    footer component done

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="contact-us">
+        <div>Contact us</div>
+        <div>
+          <a href="https://github.com/Yue-Hao14/DivvyUp-Group-Project">
+            DivvyUp 2023
+          </a>
+        </div>
+      </div>
+      <div className="contact-info">
+        <div className="contact-name">
+          Nick Arakaki
+          <div className="contact-links">
+            <a href="#">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        <div className="contact-name">
+          Troy Lee
+          <div className="contact-links">
+            <a href="#">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        <div className="contact-name">
+          Yue Hao
+          <div className="contact-links">
+            <a href="#">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        <div className="contact-name">
+          Tuan Tran
+          <div className="contact-links">
+            <a href="#">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
