78ebcbdcde77a14057994e1b41b7a271b922dacb
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-16 19:27:45 -0500

    resolve merge conflict

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -7,14 +7,6 @@ import CommentFormModal from './CommentModalComponents/CommentFormModal'
 import DeleteCommentConfirmationModal from './CommentModalComponents/DeleteCommentConfirmationModal'
 import './Comments.css'
 
-function Comments ({ expenseId }) {
-  const dispatch = useDispatch()
-  const sessionUser = useSelector(state => state.session.user)
-  const comments = useSelector(state => state.comments[expenseId])
-  // order comments by createdAt date?
-  console.log('List of comments for the expense=====================', comments)
-
-<<<<<<< HEAD
 function Comments({ expenseId }) {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user)
@@ -24,11 +16,10 @@ function Comments({ expenseId }) {
     const orderedComments = comments?.sort((a,b) => {
         return (new Date(a.createdAt)).valueOf() - (new Date(b.createdAt)).valueOf()
     })
-=======
-  useEffect(() => {
-    dispatch(getExpenseCommentsThunk(expenseId))
-  }, [dispatch])
->>>>>>> main
+
+    useEffect(() => {
+      dispatch(getExpenseCommentsThunk(expenseId))
+    }, [dispatch])
 
   return (
     <>
@@ -41,37 +32,6 @@ function Comments({ expenseId }) {
             const fomattedCreatedAt = getMMDDYYYY(new Date(comment.createdAt))
             const fomattedUpdatedAt = getMMDDYYYY(new Date(comment.updatedAt))
 
-<<<<<<< HEAD
-    return (
-        <>
-            <div className="comments_heading">COMMENTS</div>
-            {orderedComments && orderedComments.map(comment => {
-                const fomattedCreatedAt = getMMDDYYYY(new Date(comment.createdAt))
-                const fomattedUpdatedAt = getMMDDYYYY(new Date(comment.updatedAt))
-
-                return (
-                    <div key={comment.id} className="comment_div">
-                        {(sessionUser.id === comment.user.id) && (
-                            <div className="comment_edit_delete_buttons_div">
-                                <OpenModalButton
-                                    buttonText={<i className="edit_comment_button fa-solid fa-pen-to-square" />}
-                                    modalComponent={<CommentFormModal expenseId={expenseId} commentId={comment.id} />}
-                                />
-                                <OpenModalButton
-                                    buttonText={<i className='remove_comment_button fa-solid fa-trash' />}
-                                    modalComponent={<DeleteCommentConfirmationModal comment={comment} />}
-                                />
-                            </div>
-                        )}
-                        <div className="comment">{comment.comment}</div>
-                        <div className="comment_details_div">
-                            <div className="comment_author">{comment.user.firstName} {comment.user.lastName[0]}.</div>
-                            <div className="comment_timestamp_div">
-                                <div className="comment_timestamp">Posted On: {fomattedCreatedAt}</div>
-                                <div className="comment_timestamp">Last Edited: {fomattedUpdatedAt}</div>
-                            </div>
-                        </div>
-=======
             return (
               <div key={comment.id} className='comment_div'>
                 {sessionUser.id === comment.user.id && (
@@ -113,7 +73,6 @@ function Comments({ expenseId }) {
                   <div className='comment_timestamp_div'>
                     <div className='comment_timestamp'>
                       Posted On: {fomattedCreatedAt}
->>>>>>> main
                     </div>
                     <div className='comment_timestamp'>
                       Last Edited: {fomattedUpdatedAt}
