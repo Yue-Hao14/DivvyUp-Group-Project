f01ef563d401ff88bbdbb79bf7c6b3b07ddde852
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-17 15:37:03 -0500

    refactor logic in ExpenseSummaries to check if there is a friend, and if there is to find the expesnses where neither the user nor friend are in the settledOwers for each expense in current expense summaries

--- react-app/src/components/ExpenseSummaries/index.js
+++ react-app/src/components/ExpenseSummaries/index.js
@@ -4,44 +4,37 @@ import { groupExpensesByMonth } from "../../utils/expenseHelpers";
 import ExpenseSummarySection from "./ExpenseSummarySection";
 
 function ExpenseSummaries() {
-    const expenses = useSelector(state => state.expenses.currentExpenseSummaries)
-    const orderedExpenses = groupExpensesByMonth(Object.values(expenses));
+    const expenses = useSelector(state => Object.values(state.expenses.currentExpenseSummaries))
+    const sessionUser = useSelector(state => state.session.user);
     const friendId = useParams().friendId
-    const expenseArr = Object.values(orderedExpenses)[0]
 
-    // gathering unsettled expense between user and this friend
-    let unsettledExpenseArr = []
-    if (expenseArr) {
-        expenseArr.forEach(expense => {
-            const settledOwersArr = expense.settledOwers
+    let orderedExpenses = [];
+    if (friendId) {
+        // do logic to get only unsettled expenses between friend and user
+        const unsettledFriendExpenses = expenses.filter(expense => {
 
-            // if there is at least one settledOwer, we check if this friend is part of settledOwers
-            if (settledOwersArr.length > 0) {
-                let settledOwersIds = settledOwersArr.map(settledOwer => settledOwer.settledUserId)
+            const settledOwers = expense.settledOwers.map(settledOwer => settledOwer.settledUserId)
 
-                // if friend is not part of the settledOwers, we add this expense to unsettledExpenseArr
-                if (!settledOwersIds.includes(Number(friendId))) unsettledExpenseArr.push(expense)
-            } else {
-                // when there is no settled owers at all, just add this expense to unsettledExpenseArr
-                unsettledExpenseArr.push(expense)
+            if (!(settledOwers.includes(sessionUser.id) || settledOwers.includes(Number(friendId)))) {
+                return expense;
             }
-        })
+        });
+
+        orderedExpenses = groupExpensesByMonth(unsettledFriendExpenses);
+
+    } else {
+        orderedExpenses = groupExpensesByMonth(expenses);
     }
-    // console.log("unsettledExpenseArr",unsettledExpenseArr)
 
     return (
         <div className="expense_summaries_div">
-            { // check if we are on friend page
-                friendId ?
-                    // if on friend page, only show unsettledExpense with this friend
-                    unsettledExpenseArr.length > 0 ?
-                    <ExpenseSummarySection expenses={unsettledExpenseArr} /> :
-                    <h1 className="no_pending_expenses">No pending expenses</h1>
-                    // if we are not on a friend page(i.e. on "All Expenses" page, then show settled and unsettled expenses)
-                    :
-                    Object.values(orderedExpenses).map((expenseList, idx) => {
-                        return (<ExpenseSummarySection key={idx} expenses={expenseList} />)
-                    })}
+            {Object.values(orderedExpenses).length > 0 ?
+                Object.values(orderedExpenses).map((expenseList, idx) => {
+                    return <ExpenseSummarySection key={idx} expenses={expenseList} />
+                })
+            :
+                <h1 className="no_pending_expenses">No pending expenses</h1>
+            }
         </div>
     )
 }
