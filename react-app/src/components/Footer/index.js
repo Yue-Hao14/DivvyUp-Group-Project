b54301e47165bc8b159dce8f278dab48cb5932a2
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-18 17:29:50 -0500

    add styling to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,45 +3,113 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="contact-us">
-        <div>About Us</div>
-        <div>
-          <a href="https://github.com/Yue-Hao14/DivvyUp-Group-Project" target="_blank" rel="noreferrer">
-            DivvyUp 2023
-          </a>
-        </div>
+    // <footer className="footer-container">
+    //   <div className="about_us">
+    //     <div className="about_us_title">About Us</div>
+    //     <div className="about_us_link_div">
+    //         Project Repo
+    //       <a href="https://github.com/Yue-Hao14/DivvyUp-Group-Project" target="_blank" rel="noreferrer">
+    //         <i class="contact_us_github_icon fa-brands fa-github" />
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="contact-name">
+    //     Nick Arakaki
+    //     <div className="contact-links">
+    //       <a href="https://www.linkedin.com/in/nicholas-arakaki-10aa66149" target="_blank" rel="noreferrer">
+    //         <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+    //       </a>
+    //       <a href="https://github.com/NickArakaki" target="_blank" rel="noreferrer">
+    //         <i class="contact_us_github_icon fa-brands fa-github" />
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="contact-name">
+    //     Troy Lee
+    //     <div className="contact-links">
+    //       <a href="#" target="_blank" rel="noreferrer">
+    //         <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+    //       </a>
+    //       <a href="https://github.com/troybloy" target="_blank" rel="noreferrer">
+    //         <i class="contact_us_github_icon fa-brands fa-github" />
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="contact-name">
+    //     Yue Hao
+    //     <div className="contact-links">
+    //       <a href="#" target="_blank" rel="noreferrer">
+    //         <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+    //       </a>
+    //       <a href="https://github.com/Yue-Hao14" target="_blank" rel="noreferrer">
+    //         <i class="contact_us_github_icon fa-brands fa-github" />
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="contact-name">
+    //     Tuan Tran
+    //     <div className="contact-links">
+    //       <a href="#" target="_blank" rel="noreferrer">
+    //         <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+    //       </a>
+    //       <a href="https://github.com/kutun0901" target="_blank" rel="noreferrer">
+    //         <i class="contact_us_github_icon fa-brands fa-github" />
+    //       </a>
+    //     </div>
+    //   </div>
+    // </footer>
+    <footer className="footer_container">
+      <div className="about_us_title_container">
+        <div className="about_us_title">About Us</div>
       </div>
-      <div className="contact-info">
-        <div className="contact-name">
-          Nick Arakaki
-          <div className="contact-links">
+      <div className="about_us_container">
+        <div className="project_repo_container">
+          <div className="project_repo_title">Project Repo</div>
+            <a href="https://github.com/Yue-Hao14/DivvyUp-Group-Project" target="_blank" rel="noreferrer">
+              <i className="contact_us_github_icon fa-brands fa-github" />
+            </a>
+        </div>
+        <div className="about_us_dev_container">
+          <div className="about_us_dev_name">Nick Arakaki</div>
+          <div className="about_us_dev_contact_links">
             <a href="https://www.linkedin.com/in/nicholas-arakaki-10aa66149" target="_blank" rel="noreferrer">
-              LinkedIn
+              <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+            </a>
+            <a href="https://github.com/NickArakaki" target="_blank" rel="noreferrer">
+              <i class="contact_us_github_icon fa-brands fa-github" />
             </a>
           </div>
         </div>
-        <div className="contact-name">
-          Troy Lee
-          <div className="contact-links">
-            <a href="#" target="_blank" rel="noreferrer">
-              LinkedIn
+        <div className="about_us_dev_container">
+          <div className="about_us_dev_name">Troy Lee</div>
+          <div className="about_us_dev_contact_links">
+            <a href="https://www.linkedin.com/in/nicholas-arakaki-10aa66149" target="_blank" rel="noreferrer">
+              <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+            </a>
+            <a href="https://github.com/troybloy" target="_blank" rel="noreferrer">
+              <i class="contact_us_github_icon fa-brands fa-github" />
             </a>
           </div>
         </div>
-        <div className="contact-name">
-          Yue Hao
-          <div className="contact-links">
-            <a href="#" target="_blank" rel="noreferrer">
-              LinkedIn
+        <div className="about_us_dev_container">
+          <div className="about_us_dev_name">Yue Hao</div>
+          <div className="about_us_dev_contact_links">
+            <a href="https://www.linkedin.com/in/yue-hao/" target="_blank" rel="noreferrer">
+              <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+            </a>
+            <a href="https://github.com/Yue-Hao14" target="_blank" rel="noreferrer">
+              <i class="contact_us_github_icon fa-brands fa-github" />
             </a>
           </div>
         </div>
-        <div className="contact-name">
-          Tuan Tran
-          <div className="contact-links">
-            <a href="#" target="_blank" rel="noreferrer">
-              LinkedIn
+        <div className="about_us_dev_container">
+          <div className="about_us_dev_name">Tuan Tran</div>
+          <div className="about_us_dev_contact_links">
+            <a href="https://www.linkedin.com/in/nicholas-arakaki-10aa66149" target="_blank" rel="noreferrer">
+              <i className="contact_us_linkedin_icon fa-brands fa-linkedin" />
+            </a>
+            <a href="https://github.com/kutun0901" target="_blank" rel="noreferrer">
+              <i class="contact_us_github_icon fa-brands fa-github" />
             </a>
           </div>
         </div>
