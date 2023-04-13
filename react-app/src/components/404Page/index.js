81ade286fed930e41999fd237d02b1d092562060
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-13 16:03:59 -0500

    styling is getting closer

--- react-app/src/components/404Page/index.js
+++ react-app/src/components/404Page/index.js
@@ -3,9 +3,11 @@ import "./404Page.css";
 
 function PageNotFound() {
     return (
-        <div className="page-not-found-div">
+        <div className="page-not-found-wrapper">
             <h2>Looks Like The Page You're Looking For Doesn't Exist.</h2>
             <Link to="/">Return Home!</Link>
+            <div className="page-not-found-background">
+            </div>
         </div>
     )
 }
