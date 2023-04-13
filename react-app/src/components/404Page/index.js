c4e67f9b10b8148aa6e523b5eda49de2bd2107dd
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-13 17:01:58 -0500

    404 page in decent spot for now

--- react-app/src/components/404Page/index.js
+++ react-app/src/components/404Page/index.js
@@ -4,11 +4,10 @@ import "./404Page.css";
 function PageNotFound() {
     return (
         <div className="page-not-found-wrapper">
-            <div className="page-not-found-content">
-                <h2>Looks Like The Page You're Looking For Doesn't Exist.</h2>
-                <Link to="/">Return Home!</Link>
-            </div>
             <div className="page-not-found-background">
+                <div className="page-not-found-content-div">
+                    <h2>404 Page Not Found</h2>
+                </div>
             </div>
         </div>
     )
