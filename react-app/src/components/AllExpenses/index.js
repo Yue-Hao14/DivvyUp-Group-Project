f0719669b4b026f704f4ee867c22be4d807afa26
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-04-17 18:33:42 -0400

    add loading to all expenses page

--- react-app/src/components/AllExpenses/index.js
+++ react-app/src/components/AllExpenses/index.js
@@ -2,8 +2,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import TotalBalance from '../TotalBalance'
 import { getAllExpensesThunk } from '../../store/expenses'
 import ExpenseSummaries from '../ExpenseSummaries'
+import { useState } from 'react'
 
 import './AllExpenses.css'
+import Loading from '../Loading'
+import { useEffect } from 'react'
 
 function AllExpenses() {
   const currentExpenseSummaries = useSelector(
@@ -11,25 +14,33 @@ function AllExpenses() {
   )
   const allExpenses = useSelector(state => state.expenses.allExpenses)
   const dispatch = useDispatch()
+  const [isLoaded, setIsLoaded] = useState(false)
 
   // if the current expense summaries slice of state does not equal
   // all expenses slice of state dispatch thunk
   // possibly due to adding a new expense
   // or being on a friend's expense page prior
-  if (currentExpenseSummaries !== allExpenses) {
-    dispatch(getAllExpensesThunk())
-  }
+  useEffect(() => {
+    if (currentExpenseSummaries !== allExpenses) {
+      dispatch(getAllExpensesThunk())
+        .then(() => setIsLoaded(true))
+    } else setIsLoaded(true)
+  },[dispatch])
 
   return (
     <>
-      <div className='all_expenses_header_container'>
-        <div className='all_expenses_header_label'>
-          <i className='fa-solid fa-coins all_expenses_icon'></i>
-          <div className='all_expenses_header'>All Expenses</div>
-        </div>
-      </div>
-      <ExpenseSummaries />
-      <TotalBalance />
+      {isLoaded ? (
+        <>
+          <div className='all_expenses_header_container'>
+            <div className='all_expenses_header_label'>
+              <i className='fa-solid fa-coins all_expenses_icon'></i>
+              <div className='all_expenses_header'>All Expenses</div>
+            </div>
+          </div>
+          <ExpenseSummaries />
+          <TotalBalance />
+        </>
+      ) : <Loading />}
     </>
   )
 }
