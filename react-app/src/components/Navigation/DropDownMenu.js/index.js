b9e1b34f3897ab31c45d4bf90e4e53a7ca8f3f0f
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-18 14:12:07 -0500

    saving current work before lunch, need to add links to friends etc.

--- react-app/src/components/Navigation/DropDownMenu.js/index.js
+++ react-app/src/components/Navigation/DropDownMenu.js/index.js
@@ -1,6 +1,10 @@
 import { useState, useRef } from "react";
 import { useEffect } from "react";
+import OpenModalButton from "../../OpenModalButton";
+import AddFriendModal from "../../SideBar/AddFriendModal";
+import RemoveFriendModal from "../../SideBar/RemoveFriendModal";
 import "./DropDownMenu.css"
+import { NavLink } from "react-router-dom";
 
 function DropDownMenu() {
     const [showSideBar, setShowSideBar] = useState(false);
@@ -28,6 +32,10 @@ function DropDownMenu() {
     const sideBarClassName = "sidebar-dropdown" + (showSideBar ? "" : " hidden");
     const closeSideBar = () => setShowSideBar(false);
 
+    const displayFriends = () => {
+      alert("SHOW ME WHAT YOU GOT!")
+    }
+
     return (
       <>
         <button onClick={openSideBar} className='drop_down_menu_button'>
@@ -35,13 +43,18 @@ function DropDownMenu() {
         </button>
         <ul className={sideBarClassName} ref={sideBarRef}>
           <>
-            <li onClick={closeSideBar}>option</li>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
-            <li>option</li>
+            <li onClick={closeSideBar}>
+              <NavLink activeClassName='active_sidebar_link' exact to="/">Dashboard</NavLink>
+            </li>
+            <li onClick={closeSideBar}>
+              <NavLink to={`/all-expenses`}>All Expenses</NavLink>
+            </li>
+            <li onClick={closeSideBar}>
+            <NavLink to={`/payment-history`}>Payment History</NavLink>
+            </li>
+            <li onClick={displayFriends}>
+              FRIENDS
+            </li>
           </>
         </ul>
       </>
